46a4a4e83240527fb84b3e4175f6f68614ae23e5
ris8z <peppee455@gmail.com>
2024-04-04 23:48:40 +0100

    .

--- example.c
+++ example.c
@@ -24,10 +24,50 @@ int main()
 
   int *p = myFirstVarInTheHeap(1);
 
-  printf("%d\n", *p);
 
   free(p);
 
+
+
+  int matrix[3][4] ={
+    {1,2,3,4},
+    {5,6,7,8},
+    {9,10,11,12},
+  };
+
+  int C = 4;
+  int R = 3;
+  int maxsum = -10000000;
+  int sum;
+
+  for(int i = 0; i < R; i++){
+    sum = 0;
+    for(int j = 0; j < C; j++){
+        printf("%d ", matrix[i][j]);
+        sum += matrix[i][j];
+    }
+    printf("somma riga %d = %d\n", i, sum);
+    if( maxsum < sum){
+      maxsum = sum;
+    }
+  }
+  printf("Riga piu grossa %d\n", maxsum);
+
+
+  int minsum = 100000000;
+
+  for(int i = 0; i < C; i++){
+    sum = 0;
+    for(int j = 0; j < R; j++){
+        printf("%d ", matrix[j][i]);
+        sum += matrix[j][i];
+    }
+    printf("somma colnna %d = %d\n", i, sum);
+    if( minsum > sum){
+      minsum = sum;
+    }
+  }
+  printf("Riga piu piccola %d\n", minsum);
 
   return 0;
 }
